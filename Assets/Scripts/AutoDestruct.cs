2569c82e84f4e1a326466455ddcde9ab0e3667cf
Pol LEFEVRE <pol007@skynet.be>
2021-12-11 22:52:27 +0100

    Frere des our

---
+++ Assets/Scripts/AutoDestruct.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoDestruct : MonoBehaviour
+{
+	[SerializeField]
+	private float _lifetime;
+
+    // Update is called once per frame
+    void Update()
+    {
+        _lifetime -= Time.deltaTime;
+		if(_lifetime < 0){
+			Destroy(gameObject);
+		}
+    }
+}
--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -4,20 +4,22 @@ using UnityEngine;
 
 public class Target : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	[SerializeField]
+	private Transform _minPosition;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	[SerializeField]
+	private Transform _maxPosition;
 
 	void OnCollisionEnter(Collision collision) {
 		Destroy(collision.gameObject);
 		GameManager.Instance.AddPoint(10);
+
+		Vector3 newPosition = new Vector3(
+			Random.Range(_minPosition.position.x, _maxPosition.position.x),
+			Random.Range(_minPosition.position.y, _maxPosition.position.y),
+			Random.Range(_minPosition.position.z, _maxPosition.position.z)
+		);
+		
+		this.transform.position = newPosition;
 	}
 }
