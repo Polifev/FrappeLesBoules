9b95dc70e22dbff131d3cac11092cff77566f296
Pol LEFEVRE <pol007@skynet.be>
2021-12-11 11:28:45 +0100

    Add paddle

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+	public static GameManager Instance {
+		get;
+		private set;
+	}
+
+	[SerializeField]
+	private Text _text;
+	private int _score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instance = this;
+		_text.text = $"Score : {_score}";
+    }
+
+	public void AddPoint(int points){
+		_score += points;
+		_text.text = $"Score : {_score}";
+	}
+}
